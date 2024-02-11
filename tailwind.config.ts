299572d351ecb7d61438a1275158d1f802c57e2e
sereja-art <sereja.artemov.art@yandex.ru>
2024-02-11 23:06:45 +0300

    features: tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: 'class',
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -8,13 +9,51 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        darkPrimary: '#21252b',
+        darkSecondary: '#333841',
+        whitePrimary: '#f6f8fc',
+        whiteSecondary: '#fff',
+        borderColor: '#efefef',
+        blockBorderColorDark: '#464646',
+        blockBorderColorLight: '#efefef',
+        secondTextColor: '#616161',
+        secondTextColorDark: '#C0C0C0',
+        accentBg: '#6767ab29',
+      },
+      maxWidth: {
+        textContainer: '680px',
+      },
+      typography: () => ({
+        custom: {
+          css: {
+            '--tw-prose-body': 'var(--light-text-color)',
+            '--tw-prose-headings': 'var(--light-text-color)',
+            '--tw-prose-links': 'var(--light-text-color)',
+            '--tw-prose-bold': 'var(--light-text-color)',
+            '--tw-prose-invert-links': 'var(--dark-text-color)',
+            '--tw-prose-invert-bold': 'var(--dark-text-color)',
+            '--tw-prose-invert-body': 'var(--dark-text-color)',
+            '--tw-prose-invert-headings': 'var(--dark-text-color)',
+            
+            // ...
+          },
+        }
+      }),
+    },
+    fontFamily: {
+      sans: ['var(--font-open-sans)'],
+      boss: ['var(--font-raleway)'],
+      playfair: ['var(--font-playfair-display)'],
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '1rem',
+        sm: '2rem',
       },
     },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
 export default config;
