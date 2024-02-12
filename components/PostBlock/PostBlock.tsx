0b2a5cb53b1b30433a18de716d8216cfcdb1c6d8
sereja-art <sereja.artemov.art@yandex.ru>
2024-02-12 22:50:39 +0300

    refactor styles

--- components/PostBlock/PostBlock.tsx
+++ components/PostBlock/PostBlock.tsx
@@ -42,7 +42,7 @@ export default async function PostBlock() {
           </div>
         </div>
         <Link
-          className="block mb-1.5 leading-snug md:text-lg text-base font-boss"
+          className="block mb-1.5 leading-snug md:text-lg text-base font-semibold"
           href={post.url}
         >
           <h3>{post.title}</h3>
