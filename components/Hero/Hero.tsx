c4af528a94af4499157f445bf3bc9faa308b54fe
sereja-art <sereja.artemov.art@yandex.ru>
2024-02-26 19:27:30 +0300

    add main link

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
               рекламе
             </p>
             <div className='p-0.5 inline-block shrink-0 lg:p-1 border border-whiteSecondary/30 rounded-full'>
-              <button
+              {/* <button
                 type="button"
                 className="flex items-center bg-[#33384142] justify-between h-full gap-6 px-4 py-1 pr-1 border rounded-full border-whiteSecondary/30 lg:pr-1 lg:gap-5 lg:px-5 shrink-0 md:border-1 lg:text-xl"
               >
@@ -38,7 +38,31 @@ const Hero = () => {
                     fill="white"
                   />
                 </svg>
-              </button>
+              </button> */}
+              <Link 
+                rel='nofollow'
+                href='https://t.me/sereja_art'
+                target='_blank'
+                className="flex items-center bg-[#33384142] justify-between h-full gap-6 px-4 py-1 pr-1 border rounded-full border-whiteSecondary/30 lg:pr-1 lg:gap-5 lg:px-5 shrink-0 md:border-1 lg:text-xl"
+              >
+                <p className="lg:mb-[0.1em]">Начать работать</p>
+                <svg
+                  className="w-auto h-[2em]"
+                  width="18"
+                  height="18"
+                  viewBox="0 0 18 18"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    className="fill-slate-50"
+                    fillRule="evenodd"
+                    clipRule="evenodd"
+                    d="M12.4049 9.45833L9.45911 12.4063C9.25547 12.6094 8.92578 12.6094 8.72265 12.4063C8.51901 12.2031 8.51901 11.8698 8.72265 11.6667L10.869 9.52083H5.35547C5.06745 9.52083 4.83464 9.28646 4.83464 9C4.83464 8.71354 5.06745 8.47916 5.35547 8.47916H10.869L8.72265 6.33333C8.51901 6.13021 8.51901 5.80206 8.72265 5.59373C8.92578 5.3906 9.25547 5.3906 9.45911 5.59373L12.4049 8.54166C12.5299 8.66666 12.569 8.83854 12.5404 9C12.569 9.16146 12.5299 9.33333 12.4049 9.45833ZM9.0013 0.666664C4.3987 0.666664 0.667969 4.39583 0.667969 9C0.667969 13.6042 4.3987 17.3333 9.0013 17.3333C13.6039 17.3333 17.3346 13.6042 17.3346 9C17.3346 4.39583 13.6039 0.666664 9.0013 0.666664Z"
+                    fill="white"
+                  />
+                </svg>
+              </Link>
             </div>
 
           </div>
