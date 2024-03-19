1ff5777c10e6f5d737c93de04cc693435934b654
sereja-art <sereja.artemov.art@yandex.ru>
2024-03-19 23:11:27 +0300

    features: add notes search

--- components/NotesList/NotesList.tsx
+++ components/NotesList/NotesList.tsx
@@ -2,9 +2,10 @@
 
 import React, { useEffect, useState } from 'react';
 import FilterPosts from '../FilterPosts/FilterPosts';
-import { NoteType } from '@/lib/types';
+import { NoteType, PostType } from '@/lib/types';
 import getCategories from '@/lib/getCategories';
 import NoteCard from '../NoteCard/NoteCard';
+import { IoCloseOutline } from 'react-icons/io5';
 
 interface CategoryType {
   category: string;
@@ -15,26 +16,67 @@ const NotesList = ({ posts }: { posts: NoteType[] }) => {
   const categories: CategoryType[] = getCategories(posts);
   const [filteredPosts, setFilteredPosts] = useState(posts);
   const [activeCategory, setActiveCategory] = useState('');
+  const [activeCategoryItems, setActiveCategoryItems] = useState<NoteType[]>([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   function onClickPosts(event: React.MouseEvent<HTMLElement>) {
     const target = event.target as Element;
     setActiveCategory(target.id);
 
     if (target.id === 'all') {
-      const filteredArr = posts;
-      setFilteredPosts(filteredArr);
+      setActiveCategoryItems(posts);
     } else {
       const filteredArr = posts.filter((post) => post.category?.includes(target.id));
-      setFilteredPosts(filteredArr);
+      setActiveCategoryItems(filteredArr);
+    }
+  }
+
+  function searchPosts(event: React.ChangeEvent<HTMLInputElement>) {
+    setSearchQuery(event.target.value);
+  }
+
+  function filterPostsBySearchQuery() {
+    if (searchQuery !== '') {
+      const filtered = activeCategoryItems.filter(
+        (posts) =>
+          posts.title.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          posts.excerpt.toLowerCase().includes(searchQuery.toLowerCase()),
+      );
+      setFilteredPosts(filtered);
+    } else {
+      searchQuery.toLowerCase() === '' && setFilteredPosts(activeCategoryItems);
     }
   }
 
   useEffect(() => {
     setActiveCategory('all');
+    setActiveCategoryItems(posts);
   }, []);
+
+  useEffect(() => {
+    filterPostsBySearchQuery();
+  }, [searchQuery, activeCategoryItems]);
 
   return (
     <>
+          <form className="mb-4 max-w-[750px] mx-auto flex md:text-4xl text-xl items-center gap-1 relative">
+          <input
+            onChange={searchPosts}
+            value={searchQuery}
+            type="search"
+            id="default-search"
+            className="w-full py-[.4em] leading-none px-[1em] border border-darkPrimary/50 dark:border-whiteSecondary/30 rounded-full bg-whiteSecondary md:pr-[75px] pr-[45px] dark:text-whitePrimary dark:bg-darkSecondary"
+            placeholder="Начните искать и вы найдете..."
+            required
+          />
+
+
+          {searchQuery && <button onClick={() => setSearchQuery('')} type="button" className="border absolute md:right-5 bg-whiteSecondary dark:bg-darkSecondary border-darkPrimary/50 dark:border-whiteSecondary/30 rounded-full aspect-square right-[.35rem] h-[2.1rem] md:h-10">
+            <IoCloseOutline size={`100%`} />
+          </button>}
+          
+      </form>
+
       <FilterPosts
         posts={filteredPosts}
         categories={categories}
@@ -43,6 +85,7 @@ const NotesList = ({ posts }: { posts: NoteType[] }) => {
       />
 
       <div className="inline-flex flex-wrap gap-2">
+        {filteredPosts.length === 0 && <p>или не найдете...</p>}
         {filteredPosts.map((note, index) => note.published && <NoteCard key={index} {...note} />)}
       </div>
     </>
